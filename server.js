9f904894457c16a2430b0fd5a3200fcd5c4c52c9
Ritesh Jorle <riteshjorle281@gmail.com>
2026-09-03 08:57:57 +0530

    Change nodemailer transport to use SMTP settings
    Updated the nodemailer transporter configuration to use SMTP settings.

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ const otpStore = new Map();
 
 // Gmail transporter
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
